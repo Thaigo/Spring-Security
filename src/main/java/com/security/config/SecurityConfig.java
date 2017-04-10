5f87b4f0cacaf6ba4cebd7040ffb47ec16b3c5c1
Thaigo Freitas <Thaigo Freitas>
2017-04-10 15:12:55 -0300

    add securityConfig e WebConfig

--- src/main/java/com/security/config/SecurityConfig.java
+++ src/main/java/com/security/config/SecurityConfig.java
@@ -2,10 +2,31 @@ package com.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableGlobalMethodSecurity( securedEnabled = true )
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/admin/**").hasRole("ADMIN")
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.usernameParameter("email")
+				.permitAll()
+				.and()
+			.logout()
+				.logoutSuccessUrl("/login?logout")
+				.permitAll();
+			
+	}
+	
+	
 
 }
